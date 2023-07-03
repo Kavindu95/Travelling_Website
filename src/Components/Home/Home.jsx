a3ecfacea59b661717d84a75482e1ba6d408f501
Kavindu95 <kavindueng95@gmail.com>
2023-07-03 14:53:42 +0530

    Add existing project files prior to the push to GitHub.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import './home.css'
 import video from '../../Assest/video.mp4'
+import{GrLocation} from 'react-icons/gr'
+import{HiFilter} from 'react-icons/hi'
+import{FiFacebook} from 'react-icons/fi'
+import{BsInstagram} from 'react-icons/bs'
+import{FaTripadvisor} from 'react-icons/fa'
+import{AiOutlineUnorderedList} from 'react-icons/ai'
+import{TbApps} from 'react-icons/tb'
 
 const Home = () => {
   return (
@@ -17,6 +24,47 @@ const Home = () => {
        <h1 className="homeTitle">
         Search Your Holidays
        </h1>
+      </div>
+
+      <div className="cardDiv grid">
+            <div className="destinationInput">
+              <label htmlFor="city">Search your destination</label>
+              <div className="input flex">
+                <input type="text" placeholder='Enter name here...'/>
+                <GrLocation className='icon'/>
+
+              </div>
+              <label htmlFor="city">Select your date</label>
+                <div className="input flex">
+                <input type="date" placeholder='Enter name here...'/>
+                </div>
+            </div>
+
+            <div className="priceInput">
+              <div className="lable_total flex">
+                <label htmlFor="price">Max Price:</label>
+                <h5 className='total'>$5000</h5>
+              </div>
+              <div className="input flex">
+                <input type='range' max='5000' min='1000'/>
+              </div>
+            </div>
+            <div className="searchOptions flex">
+            <HiFilter className="icon"/>
+            <span>MORE FILTERS</span>
+            </div>
+      </div>
+
+      <div className="homeFooterIcons flex">
+        <div className="rightIcons">
+        <FiFacebook className="icon"/>
+        <BsInstagram className="icon"/>
+        <FaTripadvisor className="icon"/>
+        </div>
+        <div className="leftIcons">
+        <AiOutlineUnorderedList className="icon"/>
+        <TbApps className="icon"/>
+        </div>
       </div>
     </div>
       
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
-import './main.css'
+import React from "react";
+import "./main.css";
+import img from "../../Assest/image1.jpg";
+import { GrLocation } from "react-icons/gr";
+import { BsClipboardCheck } from "react-icons/bs";
+
+const Data = [
+  {
+    id: 1,
+    imgSrc: img,
+    destTitle: "Bomburu Ella",
+    location: "Welimada,Srilanka",
+    grade: "Cultural Relax",
+    fees: "500$",
+    description:
+      "Bomburu Ella is a waterfall in Sri Lanka consists of several small waterfalls grouped together. The source of the falls is a lake located in the central highlands of Sri Lanka.",
+  },
+]
 
 const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <section className="main container section">
+      <div className="secTitle">
+        <h3 className="title">Most visited destinations</h3>
+      </div>
+      <div className="secContent grid">
+        {
+        Data.map(({ id, imgSrc, destTitle, location, grade, fees, description }) => {
+            return (
+              <div key={id} className="singleDestination">
+                <div className="imageDiv">
+                  <img src={imgSrc} alt={destTitle} />
+                </div>
+                <div className="cardInfo">
+                  <h4 className="destTitle">{destTitle}</h4>
+                  <span className="continent flex">
+                    <GrLocation className="icon" />
+                    <span className="name">{location}</span>
+                  </span>
+
+                  <div className="fees flex">
+                    <div className="grade">
+                      <span>   
+                        {grade}
+                        <small>+1</small>
+                      </span>
+                    </div>
+                    <div className="price">
+                      <h5>{fees}</h5>
+                    </div>
+                  </div>
+                  <div className="desc">
+                    <p>{description}</p>
+                  </div>
+                  <button className="btn flex">
+                    DETAILS<BsClipboardCheck className="icon"/>
+                  </button>
+                </div>
+              </div>
+            );
+          }
+        )}
+      </div>
+    </section>
+  );
+};
 
 export default Main
